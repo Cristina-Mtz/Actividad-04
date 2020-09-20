286f566d171c97f344736f74dccabd6b71efda9a
Cristina Martinez <maria.martinez4724@alumnos.udg.mx>
2020-09-20 12:55:07 -0500

    Se añade la funcion MOSTRAR. El codigo funciona.

--- Retroalimentacion.cpp
+++ Retroalimentacion.cpp
@@ -21,19 +21,36 @@ void capturar_enteros()
 				printf("PROMEDIO: %.1f\n",suma/5);
 		system("pause");
 	}
+void mostrar(int n, char cadena[])
+	{
+		int i;
+		for(i=0;i<=n;i++)
+		{
+			printf("%s\n",cadena);
+		}
+	system("pause"); 	
+	}
 
 main()
 	{
 		int rep=0;
+		char cadena[20];
+		int n;
 		do
 			{
 				int op;
 					printf("Seleccione una opci%cn:\n1.Capturar enteros.\n2.Mostrar.\n3.Personaje.\n4.Salir.\n",162);scanf("%d",&op);
 				switch(op)
 					{
-						case 1: capturar_enteros();
+						case 1: 
+								capturar_enteros();
 						break;
 						case 2: 
+								printf("Inserta la cadena de caracteres:\n");
+								fflush(stdin);gets(cadena);
+								printf("Inserta la cantidad de veces que se mostrara la cadena\n");
+								scanf("%d",&n);
+								mostrar(n,cadena);
 						break;
 						case 3:
 						break;
